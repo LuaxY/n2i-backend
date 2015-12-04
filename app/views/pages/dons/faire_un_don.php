fa12e4ccd243715f80bf452366da16dd31f4ed18
Said Mezhoud <said.mezhoud@voidmx.net>
2015-12-04 07:08:34 +0100

    edit dons

--- app/views/pages/dons/faire_un_don.php
+++ app/views/pages/dons/faire_un_don.php
@@ -1,19 +1,12 @@
 <?php
 Router::view('layouts/header');
 ?>
-
-<div class="descContent" align="center">
-    <br>
-    <br>
-    <br>
-    <br>
-    Indiquez ce que vous souhaitez donner:
-    <br>
-    <br>
+<div class="descContent">
+    <p class="response">Indiquez ce que vous souhaitez donner:</p>
     <form action="/dons/choisir-une-agence" method="post">
         <textarea name="don" class="inputDon" rows="8" cols="40"></textarea>
         <br>
-        <input type="submit" value="Valider le dons et prend rendez-vous">
+        <input type="submit" class="inputDon btnValid" value="Valider le don">
     </form>
 </div>
 
